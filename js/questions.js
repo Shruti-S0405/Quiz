90745624391c4a43f4a02c5ef962da19da7ee0c8
Shruti <shruti.sundar0405@gmail.com>
2024-03-15 20:35:09 +0530

    new commit

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheets",
+    options: [
+      "Computer Style Sheets",
+      "Cascading Style Sheets",
+      "Creative Style Sheets",
+      "Colorful Style Sheets"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What is the process of finding errors and fixing them within a program called?",
+    answer: "Debugging",
+    options: [
+      "Compiling",
+      "Debugging",
+      "Interpreting",
+      "Executing"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which programming language is often used for building web applications?",
+    answer: "JavaScript",
+    options: [
+      "Java",
+      "C++",
+      "JavaScript",
+      "Python"
+    ]
+  },
+  {
+    numb: 4,
+    question: "What data structure uses First-In-First-Out (FIFO) order?",
+    answer: "Queue",
+    options: [
+      "Stack",
+      "Queue",
+      "Heap",
+      "Tree"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Which sorting algorithm has the worst-case time complexity of O(n^2)?",
+    answer: "Bubble Sort",
+    options: [
+      "Merge Sort",
+      "Quick Sort",
+      "Insertion Sort",
+      "Bubble Sort"
+    ]
+  },
+];
